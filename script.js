3cfc1926fa205001b93897c35599f2735a585577
amanchahal08 <36976261+amanchahal08@users.noreply.github.com>
2018-05-11 10:11:54 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+      <script type="text/javascript">
+
+      $("nav div").click(function() {
+            $("ul").slideToggle();
+            $("ul ul").css("display", "none");
+      });
+
+      $("ul li").click(function() {
+            $("ul ul").slideUp();
+            $(this).find('ul').slideToggle();
+      });
+
+      $(window).resize(function() {
+            if($(window).width() > 768) {
+                  $("ul").removeAttr('style');
+            }
+      });
+
+      </script>
